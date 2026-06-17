1c6e1b2bcbb4185d75fa9896f74e2c2ee11b1604
annathekarategirl <annathekarategirl@gmail.com>
2026-06-17 15:18:44 -0400

    it works

--- Iterate.js
+++ Iterate.js
@@ -1,12 +1,31 @@
 //Have func for looping, stopping, chinese vs spanish,
 
 
-document.getElementById("chinese").addEventListener("click",function(){//calls looping function but passes cnlist
-    })
+//document.getElementById("chinese").addEventListener("click",function(){//calls looping function but passes cnlist
+  //  })
 
 
 function random(arr){
-const randomIndex = Math.floor(Math.random() * arr.length);
+    let randomIndex = Math.floor(Math.random() * arr.length);
     return arr[randomIndex];}
 
-console.log(random(cnlist))
+let cnButton= document.getElementById("chinese")
+let txtDisplay=document.getElementById("txtdisplay")
+let pronounceTxt=document.getElementById("pronounce")
+let meaningTxt=document.getElementById("meaning")
+//console.log(random(cnlist))
+cnButton.addEventListener("click",function(){
+    meaningTxt.textContent=""
+    pronounceTxt.textContent=""
+    let current=random(cnlist);
+    txtDisplay.textContent=current[0]
+    if(!document.getElementById("reveal")){
+    let reveal= document.createElement("button")
+    reveal.textContent="reveal"
+    reveal.id="reveal"
+    document.body.appendChild(reveal)
+    reveal.addEventListener("click",function(){pronounceTxt.textContent=current[1];
+        meaningTxt.textContent=current[2];
+        document.body.removeChild(reveal)})
+    }
+    })
